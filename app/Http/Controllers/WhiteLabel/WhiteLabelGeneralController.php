c75bb1c5aa3b7fd55124dba29c7468864255de0b
abdulbahad123 <abdulbahad.dev@gmail.com>
2026-08-31 19:27:36 +0530

    fix: import DB and Schema facades in WhiteLabelGeneralController

--- app/Http/Controllers/WhiteLabel/WhiteLabelGeneralController.php
+++ app/Http/Controllers/WhiteLabel/WhiteLabelGeneralController.php
@@ -8,6 +8,8 @@ use App\Models\AuditLog;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 
 class WhiteLabelGeneralController extends Controller
 {
